76aaa9ee0707c3559ae6b99adc4e11d934a4cde7
ML2jinmyoung <mys1458@kc-ml2.com>
2022-08-25 21:29:33 +0900

    [FIX] env 변수 정리

--- src/api/http-upload.ts
+++ src/api/http-upload.ts
@@ -8,26 +8,25 @@
 //   },
 // }
 
-import { getBaseConfig, serializeResponse } from '@/api/http-common'
+import { getBaseConfig, serializeResponse } from "@/api/http-common";
 
-// const { REACT_APP_DEV_URL } = process.env
-const { REACT_APP_PROD_URL } = process.env
+const { REACT_APP_API_URL } = process.env;
 
 const post = (url: string, data: any, options: any) =>
-  fetch(`${REACT_APP_PROD_URL}/${url}`, {
-    ...getBaseConfig('post'),
+  fetch(`${REACT_APP_API_URL}/${url}`, {
+    ...getBaseConfig("post"),
     ...options,
     body: JSON.stringify(Object.fromEntries(data)),
-  }).then(serializeResponse)
+  }).then(serializeResponse);
 
 const getCompilerOutput = ({ data, token }: any) => {
   const headers = {
-    Authorization: 'Token ' + token,
-    'Content-type':
-      'multipart/form-data; boundary=177130003042384797933296855923',
-  }
+    Authorization: "Token " + token,
+    "Content-type":
+      "multipart/form-data; boundary=177130003042384797933296855923",
+  };
 
-  return post('upload/', data, { headers })
-}
+  return post("upload/", data, { headers });
+};
 
-export default getCompilerOutput
+export default getCompilerOutput;
