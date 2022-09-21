53d0ddba739460bb2e57a77f34f07bd2129fd6e8
ML2jinmyoung <mys1458@kc-ml2.com>
2022-09-22 01:31:11 +0900

    [ADD] redux-persist로 refresh후에도 state유지

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,18 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import './index.scss'
+
+let persistor = persistStore(store)
 
 ReactDOM.render(
   <React.Fragment>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.Fragment>,
   document.getElementById('root'),
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { persistReducer } from 'redux-persist'
+// local storage에 저장
+import storage from 'redux-persist/lib/storage'
 import graphReducer from './features/graph/graphSlice'
 import authReducer from './features/auth/authSlice'
 import modeReducer from './features/mode/modeSlice'
 
+const rootReducer = combineReducers({
+  graph: graphReducer,
+  auth: authReducer,
+  mode: modeReducer,
+})
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({
-  reducer: {
-    graph: graphReducer,
-    auth: authReducer,
-    mode: modeReducer,
-  },
+  reducer: persistedReducer,
 })
