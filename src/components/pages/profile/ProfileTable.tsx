d0b63e446f0b5c8ae282f13f79c980e65b8876e5
ML2jinmyoung <mys1458@kc-ml2.com>
2022-09-21 21:46:29 +0900

    [ADD] delete modal에 data 넣어주기

--- src/components/pages/profile/ProfileTable.tsx
+++ src/components/pages/profile/ProfileTable.tsx
@@ -6,7 +6,7 @@ import styles from './profile.module.scss'
 import pagination from './pagination.module.scss'
 import buttons from '../../../Button.module.scss'
 import axios from 'axios'
-import { Modal } from 'reactstrap'
+import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 import ReactPaginate from 'react-paginate'
 const { REACT_APP_API_URL } = process.env
 
@@ -14,7 +14,10 @@ const ProfileTable = () => {
   const { token } = useAppSelector((state) => state.auth)
   const [open, setOpen] = useState(false)
   const [items, setItems] = useState<any | undefined>(undefined)
-  const [currentID, setCurrentID] = useState()
+  const [currentID, setCurrentID] = useState<number>()
+  const [currentDate, setCurrentDate] = useState<string>('')
+  const [currentFile, setCurrentFile] = useState<string>('')
+  const [currentOption, setCurrentOption] = useState<string>('')
 
   const navigate = useNavigate()
   const dispatch = useAppDispatch()
@@ -95,7 +98,7 @@ const ProfileTable = () => {
       <table>
         <thead>
           <tr>
-            <td>data</td>
+            <td>date</td>
             <td>file name</td>
             <td>pass option</td>
             <td>show graph</td>
@@ -122,7 +125,13 @@ const ProfileTable = () => {
                     <button
                       className={buttons.mini_yellow}
                       onClick={() => {
-                        setOpen(true), setCurrentID(i[1])
+                        setOpen(true),
+                          setCurrentID(i[1]),
+                          setCurrentDate(
+                            convertDate(i[2].substring(0, i[2].indexOf('/'))),
+                          ),
+                          setCurrentFile(i[2].substring(i[2].indexOf('/') + 1)),
+                          setCurrentOption(i[0])
                       }}
                     >
                       delete
@@ -131,11 +140,27 @@ const ProfileTable = () => {
                       isOpen={open}
                       className={styles.modal}
                       backdropClassName={styles.backdrop}
-                      backdrop={'static'}
+                      size="lg"
+                      style={{ width: '100%' }}
                     >
-                      <div>
+                      <ModalHeader>
                         Are you sure you want to delete this data?
-                        <br></br>
+                      </ModalHeader>
+                      <ModalBody className={styles.modalBody}>
+                        <table>
+                          <tr>
+                            <th>date</th>
+                            <th>file name</th>
+                            <th>pass option</th>
+                          </tr>
+                          <tr>
+                            <td>{currentDate}</td>
+                            <td>{currentFile}</td>
+                            <td>{currentOption}</td>
+                          </tr>
+                        </table>
+                      </ModalBody>
+                      <ModalFooter>
                         <button
                           className={buttons.default}
                           onClick={() => deleteGraph(currentID)}
@@ -148,7 +173,7 @@ const ProfileTable = () => {
                         >
                           No
                         </button>
-                      </div>
+                      </ModalFooter>
                     </Modal>
                   </td>
                 </tr>
